beabcd2fce1ab4757cf9e58182b8e70bec010077
Mitchel Wijt <mitchelwijt@Mitchels-Macbook-Codecentric.local>
2022-05-18 14:19:19 +0200

    feat: add docker file

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -3,6 +3,8 @@ export {
 }
 
 async function enterMessageRoom(req: any, res: any, io: any) {
+    req.setTimeout(0)
+
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', 'Transfer-Encoding': 'chunked'});
     res.write("<p>Welcome to the IRC chat room!</p>")
 
