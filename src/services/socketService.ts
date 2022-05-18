e8e9671c42c5a519fc95efa896e45e373d75048c
MitchWijt <mitchel@wijt.net>
2022-05-18 15:41:06 +0200

    style message object

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -6,11 +6,12 @@ async function enterMessageRoom(req: any, res: any, io: any) {
     req.setTimeout(0)
 
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', 'Transfer-Encoding': 'chunked'});
-    res.write("<p>Welcome to the IRC chat roomSSSSSS!</p>")
+    res.write("<p>Welcome to the IRC chat room!</p>")
 
     io.on('connection', (socket: any) => {
         socket.on("message", (arg: any, callback: any) => {
-            res.write("<p>" + arg + "</p>")
+            const data = JSON.parse(arg)
+            res.write(`<p style="color: darkblue"><span>${data.name}: </span>${data.message}</p>`)
             callback(null, 'event emitted')
         })
     })
