42d6c7ca6b6094e8f1256b3e58ad4834b33ccca0
Mitchel Wijt <mitchelwijt@Mitchels-Macbook-Codecentric.local>
2022-05-18 11:51:57 +0200

    feat: removed post route

--- src/app.ts
+++ src/app.ts
@@ -12,19 +12,6 @@ const io = new SocketServer(server)
 app.use(cors())
 app.get('/', (req: any, res: any) => enterMessageRoom(req, res, io))
 
-// this is supposed to be the client side (event bus) sending a message
-import { io as clientIo } from 'socket.io-client'
-app.get('/send-message', (req, res) => {
-    const socketClient = clientIo("http://165.232.89.237:3000/")
-    socketClient.emit("message", "this is a new message")
-
-    setTimeout(() => {
-        socketClient.disconnect()
-    }, 2000)
-
-    res.json("message sent")
-})
-
 server.listen(3000, () => console.log('listening on port 3000'))
 
 
--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -7,14 +7,9 @@ async function enterMessageRoom(req: any, res: any, io: any) {
     res.write("<p>Welcome to the IRC chat room!</p>")
 
     io.on('connection', (socket: any) => {
-        res.write('user has connected \n')
-
-        socket.on('disconnect', () => {
-            res.write("<p>A user has disconnected</p>")
-        })
-
-        socket.on("message", (arg: any) => {
+        socket.on("message", (arg: any, callback: any) => {
             res.write("<p>" + arg + "</p>")
+            callback(null, 'event emitted')
         })
     })
 }
